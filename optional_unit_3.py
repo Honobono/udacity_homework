e55d30aff549577dbbe56ddba46a43c71724dae4
Ruiying Liu <ruiyingliu@ruiyings-mbp.gateway.sonic.net>
2015-09-06 15:53:38 -0700

    Add answers to Unit 3 optional problem set

---
+++ optional_unit_3.py
@@ -0,0 +1,29 @@
+#symmetric
+def symmetric(p):
+	n = len(p)
+	for r in range(n):
+		if len(p[r]) != n:
+			return False
+		for c in range(n):
+			if p[r][c] == p[c][r]:
+				pass
+			else:
+				return False
+
+	return True
+
+
+#mean
+def list_mean(p):
+	total = 0 
+    for i in p:
+		total = total + i
+	    mean = total / float(len(p))
+    return mean
+    
+
+
+
+
+
+
