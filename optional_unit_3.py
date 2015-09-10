370b4acb926b82ad8d4621993c6b9515b68a0931
RY <liuraeying@gmail.com>
2015-09-09 20:53:25 -0700

    Update optional_unit_3.py

--- optional_unit_3.py
+++ optional_unit_3.py
@@ -25,6 +25,34 @@ def list_mean(p):
 	    mean = total / float(len(p))
     return mean
     
+#antisymmetric
+def antisymmetric(A):
+    for i in range(len(A)): # i starts to loop from 0 to 2
+        for j in range(len(A)): # j starts to loop from 0 to 2
+            if A[i][j] != -A[j][i]:
+                return False
+    return True
+    
+    
+#identity_matrix
+ # A[0][0] == 1 or A[0][-1] == 1
+ # A[1][1] == 1 or A[1][-2] == 1
+ # A[2][2] == 1 or A[2][-3] == 1
+ # A[3][3] == 1 or A[3][0] == 1
+ # index of any other element should be 0 
+ 
+def is_identity_matrix(matrix):
+    for i in range(len(matrix)):
+        if len(matrix) != len(matrix[i]):
+            return False
+        if matrix[i][i] != 1: # do not include matrix[i][-i] != 1
+            return False
+        for j in range(len(matrix)):
+            if matrix[i][j] != 0 and i != j:
+                return False
+    return True
+
+#
 
 
 
